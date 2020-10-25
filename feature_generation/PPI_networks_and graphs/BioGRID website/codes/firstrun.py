5c2713a4e8276fcbf16a30b19f4b9646ffc21ef3
noakraicer <73426913+noakraicer@users.noreply.github.com>
2020-10-25 16:03:25 +0200

    Update firstrun.py

--- feature_generation/PPI_networks_and graphs/BioGRID website/codes/firstrun.py
+++ feature_generation/PPI_networks_and graphs/BioGRID website/codes/firstrun.py
@@ -4,10 +4,10 @@ f = open('firstrun.csv', 'w')
 writer = csv.writer(f)
 
 a = 0;
-with open(filename, newline = '') as games:                                                                                          
-	game_reader = csv.reader(games, delimiter='\t')
-	for game in game_reader:	
-		if game[15] == "559292" or game[16] == "559292":
+with open(filename, newline = '') as files:                                                                                          
+	file_reader = csv.reader(files, delimiter='\t')
+	for file in file_reader:	
+		if file[15] == "559292" or file[16] == "559292":
 			print('writing line', a)
 			a+=1
 			writer.writerows([game])
